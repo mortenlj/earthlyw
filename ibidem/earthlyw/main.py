b09e411d8ac7ac2785cef30cd6cc013f9401283d
Morten Lied Johansen <mortenjo@ifi.uio.no>
2021-08-13 19:25:55 +0200

    Set up colorful logging

--- ibidem/earthlyw/main.py
+++ ibidem/earthlyw/main.py
@@ -1,5 +1,34 @@
+import logging
+import os
+import sys
+
+import colorlog
+
+
+def init_logging():
+    log_level = logging.WARNING
+    if os.getenv("EARTHLYW_VERBOSE") is not None:
+        log_level = logging.DEBUG
+    root = logging.getLogger()
+    root.setLevel(log_level)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    formatter = colorlog.TTYColoredFormatter(
+        "%(log_color)s%(levelname)-8s%(reset)s %(message)s",
+        datefmt=None,
+        stream=handler.stream,
+    )
+    handler.setFormatter(formatter)
+    root.addHandler(handler)
+
+
 def main():
-    print("Hello world")
+    init_logging()
+    log = logging.getLogger(__name__)
+    log.info("Figure out which version of earthly we need")
+    log.info("See if we happen to have the correct binary already in our cache")
+    log.info("Check which os and arch we're on")
+    log.info("Download the correct binary and put it in the cache location")
+    log.info("Exec into earthly, passing along environment and args")
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     ],
     install_requires=[
         "setuptools",
+        "colorlog<6"
     ],
     extras_require={
         "dev": [
