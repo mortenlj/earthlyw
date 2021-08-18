802ef49ce56516e7f0db7bb5efc09f782a775626
Morten Lied Johansen <mortenjo@ifi.uio.no>
2021-08-18 11:01:50 +0200

    Testing across archs has to wait

--- tests/conftest.py
+++
@@ -1,3 +0,0 @@
-
-def pytest_addoption(parser):
-    parser.addoption("--binary-name", action="store", help="name of earthly binary for this platform", default="earthly-linux-amd64")
--- tests/test_identify.py
+++ tests/test_identify.py
@@ -1,16 +1,15 @@
-import pytest
-
 from ibidem.earthlyw.identify import find_binary_name
 
+VALID_BINARIES = {
+    "earthly-darwin-amd64",
+    "earthly-darwin-arm64",
+    "earthly-linux-amd64",
+    "earthly-linux-arm64",
+    "earthly-linux-arm7",
+    "earthly-windows-amd64.exe",
+}
 
-@pytest.fixture()
-def binary_name(pytestconfig):
-    binary_name = pytestconfig.getoption("binary_name")
-    if binary_name is None:
-        pytest.fail("Binary name not set")
-    return binary_name
 
-
-def test_binary_name(binary_name):
+def test_binary_name():
     actual = find_binary_name()
-    assert binary_name == actual
+    assert actual in VALID_BINARIES
