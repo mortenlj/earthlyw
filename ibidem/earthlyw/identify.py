505459857a7633be95d4f4fed28e4f343926cade
Morten Lied Johansen <mortenjo@ifi.uio.no>
2021-08-21 20:39:30 +0200

    Add extension to windows binary name

--- ibidem/earthlyw/identify.py
+++ ibidem/earthlyw/identify.py
@@ -12,9 +12,12 @@ ARCHS = {
 def find_binary_name():
     """Figure out the name of the binary we're going to need, based on OS and Arch"""
     LOG.info("Figure out name of binary...")
+    ext = ""
     os = platform.system().lower()
+    if os == "windows":
+        ext = ".exe"
     arch = ARCHS.get(platform.machine(), platform.machine())
-    binary_name = "earthly-{}-{}".format(os, arch)
+    binary_name = "earthly-{}-{}{}".format(os, arch, ext)
     LOG.info("... %r", binary_name)
     return binary_name
 
